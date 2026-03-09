0d78a3de799444885cd99d5d347ac397cabfb155
homez-bln <marcus.kempe88@gmail.com>
2026-03-09 23:43:20 +0100

    Delete be directory

--- be/ctrl/ds-theme.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Weiterleitung zum zentralen DS Controller
-header('Location: /be/ctrl/ds-settings.php');
-exit;
--- be/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/inc/auth.php';
-session_unset();
-session_destroy();
-header('Location: /be/login.php');
-exit;
